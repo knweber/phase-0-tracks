8c6712b5dfc094ff39570a559e4f8dc9722575d4
Kris <kris.weber.1994@gmail.com>
2017-04-04 08:39:41 -0700

    initial commit

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,10 +4,10 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".insert(1,"o")
 # => “zoom”
 
 # "enhance".<???>
