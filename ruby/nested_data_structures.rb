3eb6a6def8bff3bf454bf7ccbb08e85174ed2fd5
Kris <kris.weber.1994@gmail.com>
2017-04-05 20:57:21 -0700

    add hogwarts houses and character names

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,33 @@
+hogwarts = {
+	gryffindor: {
+		Harry: {
+			lastName: "Potter",
+			hairColor: "brown",
+			description: ["messy hair", "lightning bolt scar", "green eyes"]
+		}
+		Ron: {
+			lastName: "Weasley",
+			hairColor: "red",
+			description: ["lanky", "freckles", "hungry"]
+		}
+		Hermione: {
+			lastName: "Granger",
+			hairColor: "brown",
+			description: ["kind", "intelligent", "bushy hair"]
+		}
+	hufflepuff: {
+		Cedric: {
+			lastName: "Diggory",
+			hairColor: "brown",
+			description: []
+		}
+	ravenclaw: {
+		Luna: {
+			lastName: "Lovegood"
+		}
+		Padma:
+	slytherin: {
+		Draco
+		Vincent 
+		Gregory
+}
