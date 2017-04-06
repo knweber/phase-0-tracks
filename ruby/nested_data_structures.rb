fc52aa39f84faed6f9fb684a02be9d0e703853db
Kris <kris.weber.1994@gmail.com>
2017-04-05 21:17:19 -0700

    fix puts statements for accessing elements

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -4,48 +4,54 @@ hogwarts = {
 			lastName: "Potter",
 			hairColor: "black",
 			description: ["messy hair", "lightning bolt scar", "green eyes"]
-		}
+		},
 		Ron: {
 			lastName: "Weasley",
 			hairColor: "red",
 			description: ["lanky", "freckles", "hungry"]
-		}
+		},
 		Hermione: {
 			lastName: "Granger",
 			hairColor: "brown",
 			description: ["kind", "intelligent", "bushy hair"]
-		}}
+		}},
 	hufflepuff: {
 		Cedric: {
 			lastName: "Diggory",
 			hairColor: "brown",
 			description: ["one of two Hogwarts champions", "dies"]
-		}}
+		}},
 	ravenclaw: {
 		Luna: {
 			lastName: "Lovegood",
 			hairColor: "blonde",
 			description: ["intelligent", "creative", "good friend"]
-		}
+		},
 		Padma: {
 			lastName: "Patil",
 			hairColor: "black",
 			description: ["has a twin", "went with Ron to Yule Ball"]
-		}}
+		}},
 	slytherin: {
 		Draco: {
 			lastName: "Malfoy",
 			hairColor: "blonde",
 			description: ["manipulative", "misunderstood", "just a kid"]
-		}
+		},
 		Vincent: {
 			lastName: "Crabbe",
 			hairColor: "brown",
 			description: ["short", "eats cupcakes", "crony of Malfoy"]
-		} 
+		},
 		Gregory: {
 			lastName: "Goyle",
 			hairColor: "brown",
 			description: ["tall", "also eats cupcakes", "another crony of Malfoy"]
 		}}
 }
+
+# accessing second descriptive element of Harry Potter:
+puts hogwarts[:gryffindor][:Harry][:description][1]
+
+#accessing hair color of Luna Lovegood:
+puts hogwarts[:ravenclaw][:Luna][:hairColor]
