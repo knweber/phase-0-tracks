0ae29af5950ba71c4d7f23bdb1e3faeebdc65f1f
Kris <kris.weber.1994@gmail.com>
2017-04-07 09:33:50 -0700

    clean up subl

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,9 +1,3 @@
-# INPUT: put in array and an integer to search for
-# Iterate through the array to try to find the integer
-# If the integer is present, return its index
-# If the integer is not present, return nil
-
-
 #RELEASE 0
 def search_array(arr,n)
 	index = 0
@@ -17,14 +11,8 @@ end
 
 search_array([6,2,3,9,7,1],7)
 
+
 #RELEASE 1
-
-#INPUT: number of terms in fibonacci sequence that you want to see
-#n = (n.index - 2) + (n.index - 1)
-#next integer in sequence pushed into array
-#OUTPUT: array of fibonacci numbers
-
-
 def fib(n)
   array = [0,1] # x(old b) + new b 
   a=0 
@@ -40,30 +28,4 @@ end
 
 fib(100)
 
-# 5.times do |n|
-#   result = fib(n)
-#   puts result
-# end
-  
-  
-#   sequence = [0,1,1,2]
-#   fibonacci = []
-#   index = 0
-#   while index < n
-#   fibonacci.push(sequence[-1] + sequence[-2])
-#   index += 1 
-# end
-# p fibonacci
-# end
-
-# def fib(n)
-#   answerArr = [0,1]
-#   index = 0
-#   while index < n
-# 	answerArr.push((n.index-2)+(n.index-1))
-# 	end
-# end
-
-
-#fib(3) { answerArr.push((n.index-2)+(n.index-1)) }
-
+#RELEASE 2
