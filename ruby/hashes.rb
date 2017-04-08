b5d652b69e9fce6d91d16dde0f29ee13dd607e83
Kris <kris.weber.1994@gmail.com>
2017-04-08 08:53:21 -0700

    change formatting of answer change option

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -22,14 +22,40 @@ end
 
 p client
 
-puts "Before submitting, do you need to change any of the prior data? If so, type the key. Otherwise, type 'none'."
+puts "Before submitting, do you need to change any of the prior data? If so, type the key (without the preceding colon). Otherwise, type 'none'."
 fix_anything = gets.chomp.downcase
 if fix_anything == "none"
-	break
-else
+  p client
+elsif fix_anything == "name"
+  puts "What is your new answer?"
+	client[:name] = gets.chomp 
+	p client
+elsif fix_anything == "age"
+  puts "What is your new answer?"
+	client[:age] = gets.chomp.to_i
+	p client
+elsif fix_anything == "number_of_children"
+  puts "What is your new answer?"
+	client[:number_of_children] = gets.chomp.to_i 
+	p client
+elsif fix_anything == "decor_theme"
+  puts "What is your new answer?"
+	new_answer = gets.chomp 
+	p client
+elsif fix_anything == "is_high_maintenance"
 	puts "What is your new answer?"
-	modified_answer == gets.chomp
-	client[fix_anything.to_sym] = modified_answer
+	new_answer = gets.chomp 
+	  if new_answer == "y" || "yes"
+	    client[:is_high_maintenance] = true
+    elsif new_answer == "n" || "no"
+	    client[:is_high_maintenance] = false
+    end
+	p client
 end
+	
+	
+	
+	if fix_anything == "name"
+	    client[fix_anything.to_sym] == gets.chomp
 
-p client
+end
