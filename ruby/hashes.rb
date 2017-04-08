acc1f547d8f6298f99622013cb4593b475052933
Kris <kris.weber.1994@gmail.com>
2017-04-08 08:41:26 -0700

    ask user if he/she wants to change an answer

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,10 +1,4 @@
-client = {
-	name: 
-	age:
-	number_of_children:
-	decor_theme:
-	is_high_maintenance:
-}
+client = {}
 
 puts "Thank you for using our service today. First of all, what is your client's name?"
 client[:name] = gets.chomp
@@ -19,10 +13,23 @@ puts "What is the decor theme?"
 client[:decor_theme] = gets.chomp
 
 puts "Is this client high maintenance? (y/n)"
-input = gets.chomp
-if input.downcase == "y"
+input = gets.chomp.downcase
+if input == "y" || "yes"
 	client[:is_high_maintenance] = true
-else
+elsif input == "n" || "no"
 	client[:is_high_maintenance] = false
 end
 
+p client
+
+puts "Before submitting, do you need to change any of the prior data? If so, type the key. Otherwise, type 'none'."
+fix_anything = gets.chomp.downcase
+if fix_anything == "none"
+	break
+else
+	puts "What is your new answer?"
+	modified_answer == gets.chomp
+	client[fix_anything.to_sym] = modified_answer
+end
+
+p client
