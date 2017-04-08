70a3544898851a956904258a2250f80c4260a518
Kris <kris.weber.1994@gmail.com>
2017-04-08 08:28:30 -0700

    add questions for user and variables for user input

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -5,3 +5,24 @@ client = {
 	decor_theme:
 	is_high_maintenance:
 }
+
+puts "Thank you for using our service today. First of all, what is your client's name?"
+client[:name] = gets.chomp
+
+puts "How old is your client?"
+client[:age] = gets.chomp.to_i
+
+puts "How many children does your client have?"
+client[:number_of_children] = gets.chomp.to_i
+
+puts "What is the decor theme?"
+client[:decor_theme] = gets.chomp
+
+puts "Is this client high maintenance? (y/n)"
+input = gets.chomp
+if input.downcase == "y"
+	client[:is_high_maintenance] = true
+else
+	client[:is_high_maintenance] = false
+end
+
