a70eb989820cb22a46d433543a91220afb2ee00b
Kris <kris.weber.1994@gmail.com>
2017-04-10 10:10:40 -0700

    add apology

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -16,66 +16,72 @@
 # Lick your face
 # Sleep
 
-class Puppy
 
-	def speak(n)
-		n.times do 
-			puts "Woof!"
-		end
-	end
+#Accidentally wrote code for 6.2 here, please ignore
+# class Puppy
+
+# 	def speak(n)
+# 		n.times do 
+# 			puts "Woof!"
+# 		end
+# 	end
 	
-	def roll_over
-	  puts "*rolls over*"
-	end
+# 	def roll_over
+# 	  puts "*rolls over*"
+# 	end
 	
-	def dog_years(n)
-	  puts n*7
-	end
+# 	def dog_years(n)
+# 	  puts n*7
+# 	end
 	 
-	def go_away_cat
-	  puts "Woof! Go away cat!"
-	 end
+# 	def go_away_cat
+# 	  puts "Woof! Go away cat!"
+# 	 end
 	
-end
+# 	def initialize
+# 	  puts "Initializing new puppy instance..."
+# 	end
 
-Sammy = Puppy.new
+# end
 
-Sammy.speak(10)
+# Sammy = Puppy.new
 
-Sammy.roll_over
+# Sammy.speak(10)
 
-Sammy.dog_years(9)
+# Sammy.roll_over
 
-Sammy.go_away_cat
+# Sammy.dog_years(9)
 
-class Runner
+# Sammy.go_away_cat
+
+# class Runner
   
-  def initialize
-    puts "Initializing new runner instance..."
-  end 
+#   def initialize
+#     puts "Initializing new runner instance..."
+#   end 
   
-  def old_shoes
-    puts "Time to buy new shoes!"
-  end
+#   def old_shoes
+#     puts "Time to buy new shoes!"
+#   end
   
-  def five_k_time(mile_time_in_minutes)
-    puts mile_time_in_minutes*3.1
-  end
+#   def five_k_time(mile_time_in_minutes)
+#     puts mile_time_in_minutes*3.1
+#   end
   
-end
+# end
 
-Shalane = Runner.new
-Shalane.old_shoes
-Shalane.five_k_time(5.03)
+# Shalane = Runner.new
+# Shalane.old_shoes
+# Shalane.five_k_time(5.03)
 
-runners = []
-50.times do
-  runners.push(Runner.new)
-end
+# runners = []
+# 50.times do
+#   runners.push(Runner.new)
+# end
 
-p runners
+# p runners
 
-runners.each do |n|
-  n.old_shoes
-  n.five_k_time(6)
-end
+# runners.each do |n|
+#   n.old_shoes
+#   n.five_k_time(6)
+# end
