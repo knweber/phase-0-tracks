c1741787d410a885fefcc40835f9f2fd3ecee741
Kris <kris.weber.1994@gmail.com>
2017-04-10 09:49:54 -0700

    add speak, roll over, dog years, and go away cat methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,23 @@ class Puppy
     toy
   end
 
+  def speak(n)
+	n.times do 
+		puts "Woof!"
+	end 
+  end
+	
+  def roll_over
+	puts "*rolls over*"
+  end
+	
+  def dog_years(n)
+	puts n*7
+  end
+	 
+  def go_away_cat
+	puts "Woof! Go away cat!"
+  end
+
 end
 
