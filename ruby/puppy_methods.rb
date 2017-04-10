5d32431b874c04e64057063bde0fd883f28d1e35
Kris <kris.weber.1994@gmail.com>
2017-04-10 09:56:50 -0700

    add runner and class methods from puppy.rb (accidentally wrote code in that file at first. See puppy.rb for commit history)

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -40,3 +40,35 @@ Sammy.roll_over
 Sammy.dog_years(9)
 
 Sammy.go_away_cat
+
+class Runner
+  
+  def initialize
+    puts "Initializing new runner instance..."
+  end 
+  
+  def old_shoes
+    puts "Time to buy new shoes!"
+  end
+  
+  def five_k_time(mile_time_in_minutes)
+    puts mile_time_in_minutes*3.1
+  end
+  
+end
+
+Shalane = Runner.new
+Shalane.old_shoes
+Shalane.five_k_time(5.03)
+
+runners = []
+50.times do
+  runners.push(Runner.new)
+end
+
+p runners
+
+runners.each do |n|
+  n.old_shoes
+  n.five_k_time(6)
+end
