df5d5e0faa66fbf3b796f3a4f7266acd4e9096de
Kris <kris.weber.1994@gmail.com>
2017-04-10 11:10:03 -0700

    add birthday, gender, age, and ethnicity methods for release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -15,17 +15,42 @@ class Santa
 		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		age = 0
 	end
+
+	def celebrate_birthday
+		age += 1
+		puts "Happy birthday!"
+	end
+
+	#getter methods
+
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+	#setter methods
+
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
 end
 
-# Bob = Santa.new("transwoman", "Canadian")
-# Bob.speak
-# Bob.eat_milk_and_cookies("gingersnap")
+Bob = Santa.new("transwoman", "Canadian")
+Bob.speak
+Bob.eat_milk_and_cookies("gingersnap")
+Bob.celebrate_birthday
+Bob.ethnicity
 
-santas = []
-example_genders = ["transwoman", "agender", "male", "genderqueer", "female", "trigender"]
-example_ethnicities = ["Maori", "Native American", "Russian", "Australian", "Canadian"]
-example_genders.length.times do |n|
-	santas << Santa.new(example_genders[n], example_ethnicities[n])
-end 
+
+# santas = []
+# example_genders = ["transwoman", "agender", "male", "genderqueer", "female", "trigender"]
+# example_ethnicities = ["Maori", "Native American", "Russian", "Australian", "Canadian"]
+# example_genders.length.times do |n|
+# 	santas << Santa.new(example_genders[n], example_ethnicities[n])
+# end 
 
 p santas
