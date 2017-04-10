5abe9dfb07e0c2089be244aee6ea84a3fee746bf
Kris <kris.weber.1994@gmail.com>
2017-04-10 09:59:08 -0700

    adjust spacing

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,8 +1,8 @@
 class Puppy
 
 	def fetch(toy)
-      puts "I brought back the #{toy}!"
-      toy
+		puts "I brought back the #{toy}!"
+		toy
   	end
 	
 	def speak(n)
@@ -12,19 +12,19 @@ class Puppy
 	end
 	
 	def roll_over
-	  puts "*rolls over*"
+		puts "*rolls over*"
 	end
 	
 	def dog_years(n)
-	  puts n*7
+		puts n*7
 	end
 	 
 	def go_away_cat
-	  puts "Woof! Go away cat!"
+		puts "Woof! Go away cat!"
 	 end
 	
 	def initialize
-	  puts "Initializing new puppy instance..."
+		puts "Initializing new puppy instance..."
 	end
 
 end
@@ -44,15 +44,15 @@ Sammy.go_away_cat
 class Runner
   
   def initialize
-    puts "Initializing new runner instance..."
+  	puts "Initializing new runner instance..."
   end 
   
   def old_shoes
-    puts "Time to buy new shoes!"
+  	puts "Time to buy new shoes!"
   end
   
   def five_k_time(mile_time_in_minutes)
-    puts mile_time_in_minutes*3.1
+  	puts mile_time_in_minutes*3.1
   end
   
 end
