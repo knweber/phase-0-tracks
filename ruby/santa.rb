28ec6f0cf4f739a2d60dfa7321413e0ad4769969
Kris <kris.weber.1994@gmail.com>
2017-04-10 10:15:15 -0700

    add first three methods for santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,2 +1,14 @@
 class Santa
+
+	def speak
+		puts "Ho, ho, ho! Haaaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookietype)
+		puts "That was a good #{cookietype}!"
+	end
+
+	def initialize
+		puts "Initializing Santa instance..."
+	end
 end
