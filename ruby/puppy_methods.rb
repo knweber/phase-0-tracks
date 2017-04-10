487d54d0cdeba328bc98c131bb508decba0012d7
Kris <kris.weber.1994@gmail.com>
2017-04-10 09:54:42 -0700

    add fetch method and initialize method, with corresponding driver code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,31 +1,37 @@
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
-
-  def speak(n)
-	n.times do 
-		puts "Woof!"
-	end 
-  end
+	def fetch(toy)
+      puts "I brought back the #{toy}!"
+      toy
+  	end
 	
-  def roll_over
-	puts "*rolls over*"
-  end
+	def speak(n)
+		n.times do 
+			puts "Woof!"
+		end
+	end
 	
-  def dog_years(n)
-	puts n*7
-  end
+	def roll_over
+	  puts "*rolls over*"
+	end
+	
+	def dog_years(n)
+	  puts n*7
+	end
 	 
-  def go_away_cat
-	puts "Woof! Go away cat!"
-  end
-  
+	def go_away_cat
+	  puts "Woof! Go away cat!"
+	 end
+	
+	def initialize
+	  puts "Initializing new puppy instance..."
+	end
+
 end
 
 Sammy = Puppy.new
+
+Sammy.fetch("ball")
 
 Sammy.speak(10)
 
