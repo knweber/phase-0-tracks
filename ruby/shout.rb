edd5727ef44ce1804f242b219d25d3dcfa4842e7
Kris <kris.weber.1994@gmail.com>
2017-04-11 09:05:45 -0700

    add mixin shout module and two classes for release 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,16 +1,39 @@
-module Shout
+# Release 1
+# module Shout
 
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
-	end
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
 
-	def self.yell_happily(words)
-		words + "!!!" + " :D"
-	end
+# 	def self.yell_happily(words)
+# 		words + "!!!" + " :D"
+# 	end
 
-end
+# end
 
 
 # driver code
-p Shout.yell_happily("Boo-yah")
-p Shout.yell_angrily("Crap")
+# p Shout.yell_happily("Boo-yah")
+# p Shout.yell_angrily("Crap")
+
+
+# Release 3
+ module Shout
+
+ 	def yell_angrily(words)
+ 		words + "!!!" + " :("
+ 	end
+
+ 	def yell_happily(words)
+ 		words + "!!!" + " :D"
+ 	end
+
+ end
+
+ class drill_sergeant
+ 	include Shout
+ end
+
+ class overexcited_child
+ 	include Shout
+ end
