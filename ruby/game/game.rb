f993aba2a1aa81f107acf3ffae5e7c97aad13e03
Kris <kris.weber.1994@gmail.com>
2017-04-16 18:49:29 -0700

    add statement giving the number of guesses remaining

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,30 +1,31 @@
 # RELEASE 1 -- PSEUDOCODE
 
-# INPUT: correct word (str) entered by one user, and a guessed letter
+# INPUT: correct word (str) entered by one user (gets.chomp), and a guessed letter
 # (str) entered by another user (gets.chomp). 
 # STEPS:      
 # 	1. The user sees a series of dashes showing the length of the word (e.g. p "-------"")      
 # 	2. The number of guesses allowed is equal to the length of the word plus one (word.length+1) -->
-# 	until @guess_count = word.length + 1 ...		
+# 	until @guess_count = word.length		
 # 	3. If user attempts to guess the same letter twice, that does NOT count as another guess -->
 #   @guess_count += 0    
 # 	4. If the user guesses a correct letter, the answer string replaces the dash with the corresponding letter (at
-# 	the correct index); if not, the same series of dashes is returned
-# 	5. Keep asking for guesses until @guess_count = word.length+1 (counting via @guess_count += 1)     
+# 	the correct index); if not, the same series of dashes is returned (with the previous correct letters included, if they had been guessed)
+# 	5. Keep asking for guesses until @guess_count = word.length (counting via @guess_count += 1)     
 # 	6. As the user guesses correctly, the series of dashes is replaced with the
 # 	correct letters (str[other_str] = new_str)
-# OUTPUT: return 'Congratulations! You guessed the word!' if they win within the number of allotted guesses (<word.length+1), and 'Sorry,
+# OUTPUT: return 'Congratulations! You guessed the word!' if they win within the number of allotted guesses (<word.length), and 'Sorry,
 # you lost!' if they do not guess correctly within the number of guesses
 
 # RELEASE 2 -- BUILD GAME
+
+#class declaration
+
 class HangMan
-	attr_accessor :guess_count 
-	attr_reader :is_over
-	attr_accessor :correct_guesses
+	attr_reader :guess_count 
 	attr_reader :hyphenated_answer
-	attr_reader :answer 
-	attr_reader :attempt 
-	attr_reader :location
+	attr_accessor :attempt 
+	attr_accessor :correct_guesses
+	attr_accessor :answer
 
 	def initialize(answer)
 		@answer = answer
@@ -32,8 +33,6 @@ class HangMan
 		@guess_count = 0
 		@correct_guesses = []
 		@incorrect_guesses = []
-		@is_over = false
-		@location = location
 		hyphenate
 	end
 
@@ -65,7 +64,6 @@ class HangMan
 		end
 	end
 	
-@hyphenated_str = "" #string built by adding in correct letters
 @correct_guesses
 
 	def add_letter(correct_guesses)
@@ -79,12 +77,13 @@ class HangMan
 	end
 
 	def game_over
-	  puts @guess_count
+	  
+	  puts "You have used #{@guess_count} guesses. You have #{answer.length-@guess_count} guesses left!"
 		if @guess_count <= answer.length && @hyphenated_answer == @answer
-			puts "You won!"
+			puts "Congratulations! You guessed the word!"
 			true
 		elsif @guess_count == answer.length && @hyphenated_answer != @answer
-		  puts "You're out of guesses!"
+		  puts "You're out of guesses! Sorry, game over!"
 		  true
 		else
 		  false
@@ -92,6 +91,7 @@ class HangMan
 	end
 end
 
+#driver code
 
 puts "Player One, which word do you choose?"
 chosen_word = gets.chomp
@@ -105,4 +105,3 @@ until game1.game_over == true
   game1.check_letter(guessed_letter)
   p game1.hyphenated_answer
 end
-
