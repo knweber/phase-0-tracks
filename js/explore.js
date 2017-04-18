31e3913cfbdc848ed386b62aa9a1af687f984ac7
Kris <kris.weber.1994@gmail.com>
2017-04-18 11:49:00 -0700

    add pseudocode

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+// PSEUDOCODE A REVERSE METHOD
+// Input: string ("hello")
+// 	Steps:
+// 		1. Declare method with string as the parameter
+// 		2. Split the string into individual characters
+// 		3. Create a new empty array
+// 		4. Loop through the array and add each letter to the end of the new array
+// 		5. Join all the letters together to create a string
+// Output: the string, reversed ("olleh")
