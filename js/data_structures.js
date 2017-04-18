996b659cf589c8c8c7a1f770e127577121db6fa0
Kris <kris.weber.1994@gmail.com>
2017-04-18 15:02:10 -0700

    complete release 2

--- js/data_structures.js
+++ js/data_structures.js
@@ -11,3 +11,25 @@ for (i=0;i<names.length;i++) {
 }
 
 console.log(coloredHorses);
+
+function Car(make,model,year) {
+	this.make = make;
+	this.model = model;
+	this.year = year;
+
+		if (year < 2000) {
+			console.log("The speed for this car is 100 mph");
+		} else {
+			console.log("The speed for this car is 200 mph");
+		}
+
+	this.noise = function() { console.log("VROOOOMMMMMM!"); };
+}
+
+var car1 = new Car("Chevy","Camero",2010);
+var car2 = new Car("Toyota","Corolla",1996);
+
+console.log(car1);
+car1.noise();
+console.log(car2);
+car2.noise();
