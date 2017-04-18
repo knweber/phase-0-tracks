0371a190cd39f5b33da8b0eeaf4c8e101eb9e9f6
Kris <kris.weber.1994@gmail.com>
2017-04-18 14:31:21 -0700

    complete release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,2 +1,13 @@
 var colors = ["red", "periwinkle", "orange", "green", "rainbow"];
-var names = ["Brady", "Sherman", "Crush", "Hulk", "Fairy"]
+var names = ["Brady", "Sherman", "Crush", "Hulk", "Fairy"];
+
+colors.push("violet");
+names.push("Alexander");
+
+var coloredHorses = {};
+
+for (i=0;i<names.length;i++) {
+	coloredHorses[colors[i]] = names[i];
+}
+
+console.log(coloredHorses);
