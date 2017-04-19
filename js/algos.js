b12bf1ae272b575250b1747fec62c514210824e5
Kris <kris.weber.1994@gmail.com>
2017-04-19 10:52:44 -0700

    add pseudocode for release 0

--- js/algos.js
+++ js/algos.js
@@ -1,12 +1,19 @@
 // RELEASE 0: LONGEST PHRASE
 
-Input: array of strings
-Steps:
-Output: longest string in array
+// Input: array of strings
+// Steps:
+// 	1.Declare an empty string variable
+// 	2.Loop through array
+// 	3.If string is longer than current value of the empty string variable, assign it to that variable
+// 	4. Continue until you have looped through the entire array
+// 	5. The variable declared in step 1 should have a value of the longest string in the array
+// Output: longest string in array
 
 function longestString(arr) {
 
 };
+
+longestString(["apple","yoyo","Pennsylvania","yogurt"]);
 
 
 
