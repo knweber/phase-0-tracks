2598a5298354b22e28e2280ddc8a3a3b581c1799
Kris <kris.weber.1994@gmail.com>
2017-04-19 10:48:26 -0700

    add input and output for pseudocode

---
+++ js/algos.js
@@ -0,0 +1,24 @@
+// RELEASE 0: LONGEST PHRASE
+
+Input: array of strings
+Steps:
+Output: longest string in array
+
+function longestString(arr) {
+
+};
+
+
+
+// RELEASE 1: KEY-VALUE MATCH
+
+Input: two objects
+Steps: 
+Output: 'true' if they share a key-value pair; 'false' if they do not
+
+
+// RELEASE 2: GENERATE RANDOM DATA
+
+Input: integer (e.g. 7)
+Steps:
+Output: array of strings of the given integer length (e.g. 7)
