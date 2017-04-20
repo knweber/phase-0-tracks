83a09c4586504ca69fb3f1ad74cf53ebcdda593c
Kris <kris.weber.1994@gmail.com>
2017-04-19 18:37:17 -0700

    add pseudocode for release 2

--- js/algos.js
+++ js/algos.js
@@ -19,7 +19,8 @@ function longestString(arr) {
 	return longest;
 }
 
-longestString(["apple","yoyo","Pennsylvania","yogurt"]);
+console.log(longestString(["apple","yoyo","Pennsylvania","yogurt"]));
+console.log(longestString(["ab","cd","ef","ghi","jk"]));
 
 
 
@@ -27,11 +28,63 @@ longestString(["apple","yoyo","Pennsylvania","yogurt"]);
 
 // Input: two objects
 // Steps: 
+	// 1. Iterate through both loops
+	// 2. 
 // Output: 'true' if they share a key-value pair; 'false' if they do not
 
+
+function matching(obj1,obj2) {
+	for i in obj1 {
+		for j in obj2 {
+			if (obj1[i] === obj2[j]) {
+				true;
+			}
+			else {
+				false;
+			}
+		}
+	}
+}
+
+var harry {
+	name: "Harry",
+	hairColor: "brown",
+	school: "Hogwarts"
+};
+
+var luna {
+	hairColor: "blonde",
+	school: "Hogwarts",
+	name: "Luna"
+};
+
+matching(harry,luna);
 
 // RELEASE 2: GENERATE RANDOM DATA
 
 // Input: integer (e.g. 7)
 // Steps:
-// Output: array of strings of the given integer length (e.g. 7)
+// 	1. Create array holding letters of the alphabet
+// 	2. Create empty array to hold the generated words
+// 	3. Until the computer reaches the same number of loops as the integer...
+// 	4. Generate a random number between 0 and 1
+// 	5. Multiply this random number by 10 (and round up) -- this will be the number of letters in the random string
+// 	6. Iterate through the string until the given length is reached...
+// 	7. Generate a random number between 0 and 1
+// 	8. Multiply this random number by 26 (and round up)
+// 	9. Use this number as an index to select the corresponding letter in the alphabet array
+// 	10. Push the letter into the array
+// 	11. Once all the letters for the word have been pushed into the array, join them together
+// 	12. Repeat for the remaining words, as given by the initial integer
+// Output: array of n strings with random lengths and letters
+
+
+
+
+
+
+
+
+
+
+
