2163cf47f267eb0627af27bdf6173b2350bdb5da
Kris <kris.weber.1994@gmail.com>
2017-04-26 09:03:43 -0700

    initial commit for 8.5

---
+++ databases/buildsomethingawesome.rb
@@ -0,0 +1,9 @@
+puts "Hello there! Welcome to your personalized movie rating database. Please enter the title of the last movie you saw. If you wish to exit, type 'exit.'"
+first_answer = gets.chomp
+
+puts "What rating do you believe this movie deserves?"
+rating = gets.chomp
+
+puts "Would you see this movie again? (y/n)"
+see_again = gets.chomp
+
