c239eefc12be256d877b2724301516157d5a9ffc
Kris <kris.weber.1994@gmail.com>
2017-04-26 09:06:32 -0700

    add create table command, require

--- databases/buildsomethingawesome.rb
+++ databases/buildsomethingawesome.rb
@@ -1,3 +1,19 @@
+require 'sqlite3'
+
+db = SQLite3::Database.new("movies.db")
+
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS movies(
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(255),
+    rating INT,
+    watch_again BOOLEAN
+  )
+SQL
+
+db.execute(create_table_cmd)
+
+
 puts "Hello there! Welcome to your personalized movie rating database. Please enter the title of the last movie you saw. If you wish to exit, type 'exit.'"
 first_answer = gets.chomp
 
