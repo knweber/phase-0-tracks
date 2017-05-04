df45f1bbb5bfaa369be4b8d8178413e37ad7edb8
Kris <kris.weber.1994@gmail.com>
2017-05-04 10:39:59 -0700

    add code for bonus question

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -5,15 +5,15 @@ require 'sqlite3'
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
-# write a basic GET route
-# add a query parameter
-# GET /
+# # write a basic GET route
+# # add a query parameter
+# # GET /
 get '/' do
   "#{params[:name]} is #{params[:age]} years old."
 end
 
-# write a GET route with
-# route parameters
+# # write a GET route with
+# # route parameters
 get '/about/:person' do
   person = params[:person]
   "#{person} is a programmer, and #{person} is learning Sinatra."
@@ -23,8 +23,8 @@ get '/:person_1/loves/:person_2' do
   "#{params[:person_1]} loves #{params[:person_2]}"
 end
 
-# write a GET route that retrieves
-# all student data
+# # write a GET route that retrieves
+# # all student data
 get '/students' do
   students = db.execute("SELECT * FROM students")
   response = ""
@@ -37,24 +37,21 @@ get '/students' do
   response
 end
 
-# write a GET route that retrieves
-# a particular student
-
+# # write a GET route that retrieves
+# # a particular student
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
 
-# write a GET route that retrieves
-# an address
-
+# # write a GET route that retrieves
+# # an address
 get '/contact' do
   "42 Wallaby Way, Syndey, NSW, Australia"
 end
 
-# write a GET route with 
-# a name parameter
-
+# # write a GET route with 
+# # a name parameter
 get '/great_job' do
   name = params[:name]
   if name
@@ -62,4 +59,24 @@ get '/great_job' do
   else
     "Good job!"
   end
+end
+
+# # write a GET route that
+# # adds two numbers together
+# # and returns the sum
+get '/:first/add/:second' do
+  sum = params[:first].to_i + params[:second].to_i
+  "#{sum}"
+end
+
+# BONUS: write a GET route that
+# allows user to search for students
+# studying at a particular campus
+get '/students/campus/:campus' do
+  students = db.execute("SELECT * FROM students WHERE campus=?", [params[:campus].to_s])
+  students_at_campus = ""
+  students.each do |student|
+    students_at_campus << "Name: #{student['name']}<br>"
+  end
+  students_at_campus
 end
